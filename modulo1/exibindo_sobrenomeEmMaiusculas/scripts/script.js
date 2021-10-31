6673b7691378c47d892ad621a626f5d4fce748f3
Vagner Alves dos Santos <vagner_a_dos-santos@estudante.sesisenai.org.br>
2021-10-31 15:50:48 -0300

    Usando uppercase e length

---
+++ modulo1/exibindo_sobrenomeEmMaiusculas/scripts/script.js
@@ -0,0 +1,5 @@
+var ultimoSobrenome = prompt("Olá, digite seu último sobrenome:");
+var x = ultimoSobrenome.toUpperCase();
+var y = ultimoSobrenome.length;
+
+alert(`Olá ${x}. Seu sobrenome contém ${y} letras.`);
