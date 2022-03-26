b5adff18e6e05a498f09934109736006a2a7cbd0
Vagner Alves dos Santos <vagner_a_dos-santos@estudante.sesisenai.org.br>
2022-03-26 02:45:27 -0300

    Adicionando Relógio DEVinhouse

---
+++ modulo1/Semana3/ex4_Relogio/scripts/script.js
@@ -0,0 +1,18 @@
+let paragrafo = document.querySelector('#paragrafo');
+let btn = document.querySelector('#btn');
+
+const atualiza = () => {
+    let data = new Date();
+    let hora = data.getHours();
+    let minuto = data.getMinutes();
+    
+    if (hora < 10) {
+        hora = '0' + hora;
+    }
+
+    if (minuto < 10) {
+        minuto = '0' + minuto;
+    }
+
+    paragrafo.textContent = `${hora}:${minuto}`;
+    }
